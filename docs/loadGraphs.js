b1d3c9e6489a2b4b67183736af2fc15db12e71a3
Glydric <git@miglio.dev>
2024-01-25 22:11:14 +0100

    updates

--- docs/loadGraphs.js
+++ docs/loadGraphs.js
@@ -1,23 +1,19 @@
 const repoDetailsDiv = document.createElement('div');
-repoDetailsDiv.id = 'repo-details';
+repoDetailsDiv.id = 'repo';
 document.body.appendChild(repoDetailsDiv);
 
 // Fetch repository data from GitHub REST API
-fetch('https://github.com/Glydric/FlowTime')
+fetch('https://api.github.com/repos/Glydric/FlowTime')
     .then(response => response.json())
     .then(data => {
         // Create and populate the HTML elements
-        const nameElement = document.createElement('h2');
-        nameElement.textContent = data.name;
+        const nameElement = document.createElement('p');
+        nameElement.textContent = data.owner.login+'/'+data.name;
         repoDetailsDiv.appendChild(nameElement);
 
-        const ownerElement = document.createElement('h4');
-        ownerElement.textContent = data.owner.login;
-        repoDetailsDiv.appendChild(ownerElement);
-
         const avatarElement = document.createElement('img');
-        avatarElement.id = 'avatar';
-        avatarElement.src = data.owner.avatar_url;
+        avatarElement.id = 'image';
+        avatarElement.src = data.image;
         avatarElement.alt = '';
         repoDetailsDiv.appendChild(avatarElement);
     })
